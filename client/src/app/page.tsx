671a6cca1995c0e88586c235d1ae541c0e0d2571
Vikas-Rajpurohit <vikas.rajpurohit@spit.ac.in>
2023-07-21 20:16:25 +0530

    sid_vik

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -23,46 +23,6 @@ export default function HomePage() {
 
   const { dispatch } = useRoomContext();
 
-<<<<<<< HEAD
-/**
- * !STARTERCONF info
- * ? `Layout` component is called in every page using `np` snippets. If you have consistent layout across all page, you can add it here too
- */
-
-function MyApp({
-  Component,
-  pageProps: { ...pageProps },
-  router,
-}: AppProps) {
-  return (
-    <div>
-      <CommandPalette data={commands} />
-      {/* <SessionProvider session={session}> */}
-        <Layout>
-          <ToastContainer
-            toastClassName={() =>
-              "relative flex p-1 mt-4 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer bg-hl text-bg border-2 border-hl mx-4"
-            }
-            bodyClassName={() =>
-              "flex px-2 py-2 text-sm font-primary block accent-hl"
-            }
-            closeButton={() => (
-              <MdClose className="text-bg/80 transition-colors duration-200 hover:text-bg" />
-            )}
-          />
-          <Header />
-          <AnimatePresence exitBeforeEnter>
-            <Component {...pageProps} key={router.route} />
-          </AnimatePresence>
-          {/* <RoomProvider>
-            <ChatProvider>
-              
-            </ChatProvider>
-          </RoomProvider> */}
-        </Layout>
-      {/* </SessionProvider> */}
-    </div>
-=======
   return (
     <AnimateFade>
       {/* <Seo title='Monkeytype Clone' /> */}
@@ -225,6 +185,5 @@ function MyApp({
         </section>
       </main>
     </AnimateFade>
->>>>>>> 3fd72b3046263bcc4d35ed38e51107861f8a07a4
   );
 }
