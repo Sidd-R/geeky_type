61bea6a231fc92cdc4b1f41a999a12346492a10c
Sidd-R <siddhant.rao@spit.ac.in>
2023-07-23 15:09:52 +0530

    nextjs config updated

--- client/next.config.js
+++ client/next.config.js
@@ -6,8 +6,4 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
-  // webpack(config) {
-  //     config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
-  //     return config
-  // }
 }
